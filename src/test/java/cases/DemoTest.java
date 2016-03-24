3e8800b70446a47f96dda23b6fd0995aea4f54ec
Soroke <2252501946@qq.com>
2016-03-24 16:10:48 +0800

    n

---
+++ src/test/java/cases/DemoTest.java
@@ -0,0 +1,21 @@
+package cases;
+
+import org.testng.annotations.Test;
+import webtest.core.TestCase;
+import webtest.core.page.RequestPage;
+import webtest.core.page.SponsePage;
+
+/**
+ * Created by han on 2016/3/21.
+ */
+public class DemoTest extends TestCase{
+
+    @Test
+    public void baiDuYiXia(){
+        RequestPage baidu = new RequestPage();
+        baidu.input.sendKeys("百度一下你就知道");
+        baidu.button.click();
+        //SponsePage qurey = new SponsePage();
+    }
+
+}

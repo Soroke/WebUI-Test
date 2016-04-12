92451ec2cb1af70ad3d10e8beb90d7ab8332ae11
Soroke <2252501946@qq.com>
2016-04-12 18:41:37 +0800

    04-12

--- src/main/java/webtest/core/util/getDate.java
+++ src/main/java/webtest/core/util/getDate.java
@@ -4,8 +4,15 @@ import java.util.Calendar;
 
 /**
  * Created by Administrator on 2016-04-11.
+ * 获取当前电脑时间
  */
 public class getDate {
+    /**
+     * 获取电脑时间
+     * @return 返回日期格式例如：2016-4-12
+     * 注意！注意！！注意！！！这个返回的日期是当前日期的时间加一天因我要用这样的数据所以这样封装的
+     * 例如当前系统时间为2016年4月12号，那么返回的数据为：2016-04-13
+     */
     public static String getDate() {
         Calendar c = Calendar.getInstance();//可以对每个时间域单独修改
         int year = c.get(Calendar.YEAR);
@@ -16,6 +23,10 @@ public class getDate {
         String date1 = year + "-" + monthR + "-" + dateR;
         return date1;
     }
+    /**
+     * 获取电脑时间
+     * @return 返回具体时分秒格式例如：17:36:52
+     */
     public static String getDateSceond() {
         Calendar c = Calendar.getInstance();//可以对每个时间域单独修改
         int hour = c.get(Calendar.HOUR_OF_DAY);

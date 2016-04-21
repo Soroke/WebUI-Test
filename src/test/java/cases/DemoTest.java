f050c62a1dd8e57caf1e8831cfecc7240bd5e0ed
Soroke <2252501946@qq.com>
2016-04-21 13:07:46 +0800

    0421

--- src/test/java/cases/DemoTest.java
+++ src/test/java/cases/DemoTest.java
@@ -1,17 +1,5 @@
 package cases;
-
-import cases_ejj.HomeLinkTest;
-import cases_ejj.LoginTest;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import webtest.core.TestCase;
-import webtest.core.page.RequestPage;
-import webtest.core.page.SponsePage;
-import webtest.core.page_ejj.dingdanguanli.LookAllOrderPage;
-import webtest.core.util.Wait;
-
 /**
  * Created by han on 2016/3/21.
  */

19843002169bb61cad4f764fed53b259848effc3
Soroke <2252501946@qq.com>
2016-06-13 14:59:54 +0800

    0613

---
+++ src/main/java/webtest/core/page_ejj/ayiguanli/LookAllAunt.java
@@ -0,0 +1,11 @@
+package webtest.core.page_ejj.ayiguanli;
+
+import webtest.core.Page;
+
+/**
+ * Created by Administrator on 2016-06-06.
+ * 查看所有阿姨页面
+ */
+public class LookAllAunt extends Page {
+
+}
